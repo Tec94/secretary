82a1aaf8ae22fceb692907a1e3531b0134dbb870
Tec94 <caodangkhiet1@gmail.com>
2024-06-17 14:40:38 +0700

    add function to get google calendar events automatically run voice recording script put the text transcript into prompt

--- testing scripts/script.py
+++ testing scripts/script.py
@@ -2,11 +2,14 @@ import google.generativeai as genai, os
 from load_keys import load_env
 from speech_recorder import record
 from speech_converter import speech_to_text
+from get_calendar_events import get_calendar_events
 
 load_env()
+record()
+
 genai.configure(model_api_key=os.getenv('MODEL_API_KEY'))
 
 model = genai.GenerativeModel(model_name='gemini-1.5-pro')
-response = model.generate_content('Teach me about how an LLM works')
+response = model.generate_content(speech_to_text())
 
 print(response.text)

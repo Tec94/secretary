f52d752eccd2e32530b242a9f49b4cef197dd8c3
Tec94 <caodangkhiet1@gmail.com>
2024-06-17 14:07:09 +0700

    rename file

--- testing scripts/GGcalendar.py
+++ testing scripts/GGcalendar.py
@@ -7,18 +7,21 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def main():
     creds = None
+
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json')
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
         creds = flow.run_local_server(port=0)
+
     with open("token.json", "w") as token:
         token.write(creds.to_json())
 
@@ -39,3 +42,6 @@ def main():
             print(start, event["summary"])
     except HttpError as error:
         print('An error has occured:', error)
+
+if __name__ == "__main__":
+    main()
